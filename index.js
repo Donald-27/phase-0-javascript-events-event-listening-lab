2b43d393d531b83e1482587a99365c4f8312593c
Donald-27 <+>
2025-03-01 09:32:12 +0300

    modified index.js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const button = document.getElementById('button');
+
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+
+    button.addEventListener('click', clickAlert);
 }
+
+// Don't call the function immediately; the test will likely call it.
